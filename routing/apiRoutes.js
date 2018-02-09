23677a4bdbfdb30b4024dd3c7e3ed0db54c59f32
klanza <klanza91@gmail.com>
2018-02-09 00:47:27 -0700

    Beginning layout for HTML

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -4,4 +4,8 @@ module.exports = function(app) {
     app.get('/api/friends', function(req, res) {
         res.json(friendData);
     });
+    app.post('api/friends', function(req, res) {
+        friendData.push(req.body);
+        res.json(true);
+    });
 };
