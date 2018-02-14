8e13dbd1e78bab4a28c6b7a2fda6a83f18e50191
klanza <klanza91@gmail.com>
2018-02-13 18:20:27 -0700

    Set up modal, set up matching function, unable to complete due to key value mismatch

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,13 @@
 let friendData = [
     {
-    name: 'Kenny Lanza',
-    email: 'klanza91@gmail.com',
-    photo: 'nyess',
-    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+        name: 'Kenny Lanza',
+        photo: 'https://klanza.github.io/ResponsivePortfolio/assets/images/Profile.jpg',
+        scores: [3, 5, 5, 2, 5, 3, 2, 5, 3, 4],
+    },
+    {
+        name: 'James Buffet',
+        photo: 'https://www.forbes.com/profile/jimmy-buffett/',
+        scores: [5, 5, 5, 1, 5, 5, 1, 5, 5, 5],
     },
 ];
 
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -5,7 +5,30 @@ module.exports = function(app) {
         res.json(friendData);
     });
     app.post('/api/friends', function(req, res) {
+        let userInfo = req.body;
+
+        // JSON is being parsed with an added [] in the key value, causing the value
+        // to not be accessible, and can't be manipulated. Almost there just need to fix
+        // this bug and then write answer to jQuery, logic almost complete.
+        console.log(userInfo)
+        console.log(userInfo.scores)
+        let userScore = userInfo.scores.map(function(number) {
+            return parseInt(number, 10);
+        });
+        let bestMatchPic = '';
+        let bestMatchValue = 50;
+        let currentMatchValue = '';
+        friendData.forEach((element) => {
+            for (let index = 0; index < element.scores.length; i++) {
+                currentMatchValue += Math.abs(userScore[i] - parseInt(element.scores[i]));
+            } if (currentMatchValue < bestMatchValue) {
+                currentMatchValue = bestMatchValue;
+                bestMatchName = element.name;
+                bestMatchPic = element.photo;
+            };
+            console.log(bestMatchPic, bestMatchName);
+        });
+
         friendData.push(req.body);
-        // res.json(true);
     });
 };
