aaaf3677526c308715acf955c372a12596c4f2e8
klanza <klanza91@gmail.com>
2018-02-22 18:16:32 -0700

    Fixed post method to no longer use PHP

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -7,26 +7,24 @@ module.exports = function(app) {
     app.post('/api/friends', function(req, res) {
         let userInfo = req.body;
 
-        // JSON is being parsed with an added [] in the key value, causing the value
-        // to not be accessible, and can't be manipulated. Almost there just need to fix
-        // this bug and then write answer to jQuery, logic almost complete.
-        console.log(userInfo)
-        console.log(userInfo.scores)
-        let userScore = userInfo.scores.map(function(number) {
+        // console.log(userInfo)
+        // console.log(userInfo.scores)
+        let score = userInfo.scores.map(function(number) {
             return parseInt(number, 10);
         });
         let bestMatchPic = '';
         let bestMatchValue = 50;
         let currentMatchValue = '';
         friendData.forEach((element) => {
-            for (let index = 0; index < element.scores.length; i++) {
-                currentMatchValue += Math.abs(userScore[i] - parseInt(element.scores[i]));
+            for (let i = 0; i < element.scores.length; i++) {
+                currentMatchValue += Math.abs(score[i] - parseInt(element.scores[i]));
             } if (currentMatchValue < bestMatchValue) {
                 currentMatchValue = bestMatchValue;
                 bestMatchName = element.name;
                 bestMatchPic = element.photo;
+                console.log(bestMatchPic, bestMatchName);
             };
-            console.log(bestMatchPic, bestMatchName);
+            // console.log(bestMatchPic, bestMatchName);
         });
 
         friendData.push(req.body);
